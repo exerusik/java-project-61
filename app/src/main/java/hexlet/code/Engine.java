82dee94f89da4adf816437ed8c5e99f63b53ac1c
Rusik <exe.rusik@gmail.com>
2022-09-30 11:35:27 +0300

    fix Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -29,7 +29,7 @@ public class Engine {
                             + "'" + Engine.correctAnswer[i]
                             + "'.\n"
                             + "Let's try again, "
-                            + Cli.getName());
+                            + Cli.getName() + "!");
                 break;
             }
             if (i == 2) {
