6b39d061c3ef4ebd20bacd9487dead06e1f5e61a
Rusik <exe.rusik@gmail.com>
2022-10-13 22:48:23 +0300

    clean code

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -10,8 +10,8 @@ public class Gcd {
         Random random = new Random();
         String[][] questionAndAnswer = new String[Engine.COUNT_OF_ROUND][Engine.Q_AND_A];
         for (int i = 0; i < Engine.COUNT_OF_ROUND; i++) {
-            int randOne = random.nextInt(Engine.BOUND);
-            int randTwo = random.nextInt(Engine.BOUND);
+            int randOne = random.nextInt(1, Engine.BOUND);
+            int randTwo = random.nextInt(1, Engine.BOUND);
             questionAndAnswer[i][Engine.QUESTION] = randOne + " " + randTwo;
             questionAndAnswer[i][Engine.ANSWER] = String.valueOf(gcdAlgorithm(randOne, randTwo));
 
